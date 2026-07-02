e4e35063a0f119bfa185b261dbbe8bbdb2caecce
Costas Papastathis <papastathiscr@gmail.com>
2026-07-02 16:55:22 +0200

    test: Fixing the tests reading the log output

--- test/integration/programApi/commands/build/podman.test.ts
+++ test/integration/programApi/commands/build/podman.test.ts
@@ -2,7 +2,7 @@ import {config, expect} from 'chai'
 import {execa} from 'execa'
 import {it} from 'mocha'
 import path from 'node:path'
-import {text} from 'node:stream/consumers'
+import {type Readable} from 'node:stream'
 import {fileURLToPath} from 'node:url'
 
 import {createPaicku} from '../../../../../src/index.js'
@@ -28,20 +28,40 @@ describe('programmatic API build (podman)', () => {
     const inspectResult = await paicku.inspect(containerImage.imageName, {'container-runtime': 'podman', output: 'json'})
     expect(inspectResult.parsedStdout).to.not.be.null
 
-    let started
+    let container
     try {
-      started = await containerImage.run({exposedPorts})
+      container = await containerImage.run({exposedPorts})
 
-      const response = await fetch(started.url)
+      const response = await fetch(container.url)
       expect(response.status).to.equal(200)
       expect(await response.text()).to.equal('hello world')
 
-      expect(started.logs).to.be.a('function')
-      const logOutput = await text(await started.logs())
+      expect(container.logs).to.be.a('function')
+
+      const stream = (await container.logs()) as Readable
+      const logOutput = await new Promise<string>((resolve) => {
+        let output = ''
+        const timeout = setTimeout(() => {
+          stream.destroy()
+          resolve(output)
+        }, 3000)
+
+        stream
+          .on('data', (line) => {
+            output += line.toString()
+          })
+          .on('err', (line) => {
+            output += line.toString()
+          })
+          .on('end', () => {
+            clearTimeout(timeout)
+            resolve(output)
+          })
+      })
       expect(logOutput).to.contain('server is listening on')
     } finally {
-      if (started) {
-        await started.stop()
+      if (container) {
+        await container.stop()
       }
 
       await execa('podman', ['rmi', containerImage.imageName], {reject: false})
